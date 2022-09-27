dacbee886fb81b8a53eaa9a5d540304bc310133d
LIGO <103901767+LIGO7@users.noreply.github.com>
2022-09-27 20:09:49 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Glider i regnen script af LIGO#1010'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
---
+++ client.lua
@@ -0,0 +1,28 @@
+local ped = PlayerPedId()
+local cooldown = 30000 -- Hvor lang tid der går mellem hver gang den tager chancen om personen glider. Standard 30 sekunder. Kan blive ændret. (Skrives i millisekunder)
+
+Citizen.CreateThread(function()
+while true do
+Citizen.Wait(2)
+local weather = GetWeatherTypeTransition()
+
+player = PlayerId()
+
+if weather == 0x54A69840 or weather == -1233681761 then
+if IsPlayerClimbing(player) or IsPedWalking(ped) or IsPedRunning(ped) or IsPedSprinting(ped) then -- Checker om spilleren løber, går eller sprinter
+
+Wait(cooldown)
+
+local chance = math.random(1,6) -- Vælger et tal mellem 1 og 6.
+if chance == 1 then -- Hvis tallet bliver 1 glider man.
+   
+local ForwardVector = GetEntityForwardVector(ped)
+SetPedToRagdollWithFall(ped, 1500, 2000, 0, ForwardVector, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+exports['mythic_notify']:DoHudText('error', 'Du glider!')
+Wait(cooldown)
+
+end
+end
+end
+end
+end)
